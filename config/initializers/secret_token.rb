3a06412c4444eb283e507311cd05767730396055
Andreea <andreea@eXpertBOOK.(none)>
2013-03-18 21:27:38 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cazari::Application.config.secret_token = 'f592057a97a7f6cb67e492db4a2497e1995b91800049101f8371e950246b029238fa2947285ebe58d093196e189e398276d8b1481d86977af102fda4755ad9a7'
