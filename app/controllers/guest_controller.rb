e410521c477e7498b79528360dad22e1cc0ad5ac
andreeaanghelache <andreea.anghelache@gmail.com>
2013-06-09 23:30:03 +0300

    fix in guest/index

--- app/controllers/guest_controller.rb
+++ app/controllers/guest_controller.rb
@@ -1,14 +1,14 @@
 class GuestController < ApplicationController
 
  def index
- #logger.info("current user=" + current_user.to_s)
+ logger.info("current user=" + current_user.inspect)
    if current_user
      if current_user.is_student == "true"
        respond_to do |format|
                 format.html { redirect_to '/profil_student' }
        end
      end
-     if current_user.is_management = "true"
+     if current_user.is_management == "true"
        respond_to do |format|
        		format.html { redirect_to '/lista_cereri'}
        end
